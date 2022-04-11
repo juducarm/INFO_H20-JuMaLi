10e0e62d0167ef1ab813015e0b6416e84907d00a
Juducarm <jujducrm@gmail.com>
2022-04-11 16:53:03 +0200

    Initial commit

---
+++ app/src/main/java/be/julien/info_h20_jml/MainActivity.kt
@@ -0,0 +1,11 @@
+package be.julien.info_h20_jml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
