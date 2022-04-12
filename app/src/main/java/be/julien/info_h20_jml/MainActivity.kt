8f2e1b3be1d7ca194a50ee6a0813efe88f679ede
Juducarm <jujducrm@gmail.com>
2022-04-12 18:01:55 +0200

    V0

--- app/src/main/java/be/julien/info_h20_jml/MainActivity.kt
+++ app/src/main/java/be/julien/info_h20_jml/MainActivity.kt
@@ -2,10 +2,17 @@ package be.julien.info_h20_jml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.boutonPrincipal -> {
+                if (boutonPrincipal.text )
+            }
+
+
+        }
     }
 }
