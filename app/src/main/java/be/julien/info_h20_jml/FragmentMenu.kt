fa983fa778d87d15798c91b6d9f1a1c853f89131
Juducarm <jujducrm@gmail.com>
2022-04-12 20:36:42 +0200

    V1

---
+++ app/src/main/java/be/julien/info_h20_jml/FragmentMenu.kt
@@ -0,0 +1,55 @@
+package be.julien.info_h20_jml
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentMenu: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        //inflater transforme le xml en view
+        val view = inflater!!.inflate(R.layout.fragment_menu,container,false)
+
+        val tv = view.findViewById<TextView>(R.id.fragment_menu)
+
+
+        return view
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+}
--- app/src/main/java/be/julien/info_h20_jml/MainActivity.kt
+++ app/src/main/java/be/julien/info_h20_jml/MainActivity.kt
@@ -2,17 +2,54 @@ package be.julien.info_h20_jml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
+import android.view.WindowManager
+import android.view.WindowManager.LayoutParams.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
-    override fun onClick(v: View) {
-        when(v.id) {
-            R.id.boutonPrincipal -> {
-                if (boutonPrincipal.text )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN); //cachage de la barre de status
+        setContentView(R.layout.activity_main)
+
+
+        //bouton de changement de fragment
+        bouton_principal.setOnClickListener {
+            if (bouton_principal.text == getString(R.string.afficher_clavier)) {
+                bouton_principal.text = getString(R.string.afficher_menu )
+
+                val fragmentClavier = FragmentClavier()  //creation du fragment clavier
+
+                val manager = supportFragmentManager //appel au gestionnaire de fragment
+
+                //transaction vers le nouveau fragment
+                val transaction = manager.beginTransaction()
+                transaction.replace(R.id.fragment_container,fragmentClavier)
+                transaction.addToBackStack(null) //conserve le fragment en mémoire
+                transaction.commit()
+
             }
 
+            else {
+                bouton_principal.text = getString(R.string.afficher_clavier)
+                val fragmentMenu = FragmentMenu()  //creation du fragment clavier
+
+                val manager = supportFragmentManager //appel au gestionnaire de fragment
+
+                //transaction vers le nouveau fragment
+                val transaction = manager.beginTransaction()
+                transaction.replace(R.id.fragment_container,fragmentMenu)
+                transaction.addToBackStack(null) //conserve le fragment en mémoire
+                transaction.commit()
+            }
 
         }
+
     }
+
+
+
 }
